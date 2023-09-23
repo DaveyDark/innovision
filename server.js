7903afbdfab1422822168066243ab3449d85049e
Mayank <mayank771768@gmail.com>
2023-09-23 10:26:11 +0530

    Navbar and Cricket page

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const session = require('express-session')
-const bodyParser = require('body-parser'); // Middleware for parsing request bodies
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser"); // Middleware for parsing request bodies
 
-const api = require('./routes/api')
+const api = require("./routes/api");
 
-app = express()
+app = express();
 
-app.use(express.json())
-app.use(express.static('static'))
+app.use(express.json());
+app.use(express.static("static"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api',api)
-app.use(session({
-    secret: 'finance',
+app.use("/api", api);
+app.use(
+  session({
+    secret: "finance",
     resave: false,
-    saveUninitialized: true
-}));
-app.set('view engine', 'pug')
+    saveUninitialized: true,
+  })
+);
+app.set("view engine", "pug");
 
-app.get('/', (req,res) => {
-  res.render('index')
-})
-app.get('/login', (req,res) => {
-  res.render('login')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.listen(5000)
+app.listen(5000);
