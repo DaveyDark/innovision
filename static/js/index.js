8a3921fec8a55e338c2ae2b50265f8d6efba71aa
DaveyDark <devesh97531@gmail.com>
2023-09-30 18:22:23 +0530

    Homepage layout done

--- static/js/index.js
+++ static/js/index.js
@@ -12,3 +12,21 @@ $(document).ready(() => {
     $('.navbar').toggleClass('bg-transparent', !shouldAddClass);
   });
 });
+
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 2000,
+  },
+});
